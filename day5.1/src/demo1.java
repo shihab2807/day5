09f390b1ae831e2040535236afad547ce4d1131b
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 07:17:24 +0100

    day5 done

---
+++ day5.1/src/demo1.java
@@ -0,0 +1,12 @@
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		float f = 10.2525f;
+		int l = (int)f;
+		System.out.println(l);
+		// TODO Auto-generated method stub
+
+	}
+
+}
